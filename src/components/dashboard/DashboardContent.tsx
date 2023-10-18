b50421eacde01a141076a8e5ffa9125e61ade28c
Syrnnik <sakh.alexandr@gmail.com>
2023-10-19 01:43:56 +0300

    style: add TODO for spinner in dashboard

--- src/components/dashboard/DashboardContent.tsx
+++ src/components/dashboard/DashboardContent.tsx
@@ -12,13 +12,14 @@ import useFetchProjects from "~/lib/projects/hooks/use-fetch-projects";
 
 const DashboardContent = () => {
 	const userSession = useUserSession();
-	const userData = userSession!.data;
+	const userData = userSession!.data!;
 
-	const { data: projectsList, status } = useFetchProjects(userData!.id);
+	const { data: projectsList, status } = useFetchProjects(userData.id!);
 
 	return (
 		<div className={"flex flex-col space-y-6 pb-36"}>
 			{status === "loading" ? (
+				// TODO: center this spinner
 				<Spinner />
 			) : (
 				<ProjectsTable projects={projectsList} />
