0ccfbfd73d1d3e2f8b6bb7264a84a429c6b093f8
Syrnnik <sakh.alexandr@gmail.com>
2023-10-19 01:33:31 +0300

    styles: rename files & format them

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,26 +1,26 @@
-import { GetServerSidePropsContext } from 'next';
-import dynamic from 'next/dynamic';
+import { GetServerSidePropsContext } from "next";
+import dynamic from "next/dynamic";
 
-import { withAppProps } from '~/lib/props/with-app-props';
-import RouteShell from '~/components/RouteShell';
+import RouteShell from "~/components/RouteShell";
+import { withAppProps } from "~/lib/props/with-app-props";
 
-const DashboardDemo = dynamic(
-  () => import('~/components/dashboard/DashboardDemo'),
-  {
-    ssr: false,
-  }
+const DashboardContent = dynamic(
+	() => import("~/components/dashboard/DashboardContent"),
+	{
+		ssr: false,
+	},
 );
 
 const Dashboard = () => {
-  return (
-    <RouteShell title={'Dashboard'}>
-      <DashboardDemo />
-    </RouteShell>
-  );
+	return (
+		<RouteShell title={"Dashboard"}>
+			<DashboardContent />
+		</RouteShell>
+	);
 };
 
 export default Dashboard;
 
 export async function getServerSideProps(ctx: GetServerSidePropsContext) {
-  return await withAppProps(ctx);
+	return await withAppProps(ctx);
 }
