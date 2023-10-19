236755f37922b2f4059ea61866c2f43e2a729b84
Syrnnik <sakh.alexandr@gmail.com>
2023-10-19 22:17:39 +0300

    feat: init flagsmith

---
+++ src/core/flagsmith/hooks/init-flagsmith.ts
@@ -0,0 +1,15 @@
+import flagsmith from "flagsmith";
+
+const initFlagsmith = () => {
+	const apiKey = process.env.NEXT_PUBLIC_FLAGSMITH_API_KEY;
+
+	if (!apiKey) {
+		throw new Error("Flagsmith API key is not defined");
+	} else {
+		flagsmith.init({
+			environmentID: apiKey,
+		});
+	}
+};
+
+export default initFlagsmith;
