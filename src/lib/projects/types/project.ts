08e3d315840dac3980ef320bc393c517664f4ef0
Syrnnik <sakh.alexandr@gmail.com>
2023-10-20 21:20:36 +0300

    refactor: remove TODOs from Project type

--- src/lib/projects/types/project.ts
+++ src/lib/projects/types/project.ts
@@ -2,9 +2,7 @@ import { Timestamp } from "firebase/firestore";
 
 export type Project = {
   name: string;
-  // TODO: add languages we use
   language: string;
-  // TODO: add project statuses
   status: string;
   userId: string;
   createdAt: Timestamp;
