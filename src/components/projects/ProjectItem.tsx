d7b298b6dae7ef65ff50a4c9866bd3aa38c22acf
Daniyal Gabitov <danuy@list.ru>
2023-10-21 03:02:03 +0300

    Replace select of media file to tabs

--- src/components/projects/ProjectItem.tsx
+++ src/components/projects/ProjectItem.tsx
@@ -13,6 +13,7 @@ import {
 import Alert from "~/core/ui/Alert";
 import ArrowLeftIcon from "@heroicons/react/24/outline/ArrowLeftIcon";
 import Button from "~/core/ui/Button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/core/ui/Tabs"
 
 import useFetchProject from "~/lib/projects/hooks/use-fetch-project";
 
@@ -23,18 +24,6 @@ const ProjectItem: React.FC<{
 	projectId: string;
 }> = ({ projectId }) => {
 	const { data: project, status } = useFetchProject(projectId);
-
-	const [mediaSource, setMediaSource] = useState("translated");
-
-	useEffect(() => {
-		if (project) {
-			// TODO temporary added hardcoded media
-			project.output =
-				"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4";
-			project.input =
-				"https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3";
-		}
-	}, [project]);
 
 	if (status === "loading") {
 		return <PageLoadingIndicator>Loading Project...</PageLoadingIndicator>;
@@ -48,34 +37,61 @@ const ProjectItem: React.FC<{
 		);
 	}
 
+	// TODO temporary added hardcoded media
+	project.output =
+		"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4";
+	project.input =
+		"https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3";
+
 	return (
 		<div>
 			<div className={"flex flex-1"}>
-				<Button size={"small"} color={"transparent"} href={"/dashboard"}>
-					<ArrowLeftIcon className={"mr-2 h-4"} />
-					Back to Dashboard
-				</Button>
+				<div className={"absolute"}>
+					<Button size={"small"} color={"transparent"} href={"/dashboard"}>
+						<ArrowLeftIcon className={"mr-2 h-4"} />
+						Back
+					</Button>
+				</div>
 				<div className={"max-w-xs"}>
-					<Select defaultValue={mediaSource} onValueChange={setMediaSource}>
-						<SelectTrigger>{mediaSource}</SelectTrigger>
 
-						<SelectContent>
-							<SelectItem value={"translated"}>Translated</SelectItem>
-							<SelectItem value={"origin"}>Origin</SelectItem>
-						</SelectContent>
-					</Select>
 				</div>
 			</div>
 			<div className={"flex justify-center"}>
 				<div className={"flex flex-1 flex-col space-y-0.5 max-w-4xl"}>
-					<video
-						controls
-						src={mediaSource === "translated" ? project.output : project.input}
-					/>
-					<div>{project.name}</div>
-					<div>{project.language}</div>
-					<div>{project.status}</div>
-					<div>{project.createdAt.toDate().toDateString()}</div>
+					<Tabs defaultValue="translated" className="flex flex-col items-center mb-6">
+						<TabsList>
+							<TabsTrigger value="translated">Translated</TabsTrigger>
+							<TabsTrigger value="origin">Origin</TabsTrigger>
+						</TabsList>
+						<TabsContent value="translated" className="mt-6 w-full">
+							<video
+								className="w-full"
+								controls
+								src={project.output}
+							/>
+						</TabsContent>
+						<TabsContent value="origin" className="mt-6 w-full">
+							<video
+								className="w-full"
+								controls
+								src={project.input}
+							/>
+						</TabsContent>
+					</Tabs>
+					<div className="flex border rounded-lg px-6 py-5">
+						<div className="mr-6">
+							<div className="text-[8px]">Name</div>
+							<div>{project.name}</div>
+						</div>
+						<div className="mr-6">
+							<div className="text-[8px]">Target language</div>
+							<div>{project.language}</div>
+						</div>
+						<div>
+							<div className="text-[8px]">Created at</div>
+							<div>{project.createdAt.toDate().toDateString()}</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
