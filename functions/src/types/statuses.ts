fc4bf2cfe773f330bde3d0518baf3ef929307d65
Syrnnik <sakh.alexandr@gmail.com>
2023-10-22 18:22:21 +0300

    feat: add project statuses list to cloud functions

---
+++ functions/src/types/statuses.ts
@@ -0,0 +1,9 @@
+enum PROJECT_STATUSES {
+  uploading = "uploading",
+  uploaded = "uploaded",
+  uploadError = "uploadError",
+  translating = "translating",
+  translated = "translated",
+}
+
+export default PROJECT_STATUSES;
