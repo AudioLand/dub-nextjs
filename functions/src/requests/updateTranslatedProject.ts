a06a101436dfaf9587630ac8cf590249423f8b52
Syrnnik <sakh.alexandr@gmail.com>
2023-10-22 20:13:19 +0300

    feat: add logs when update translated project

--- functions/src/requests/updateTranslatedProject.ts
+++ functions/src/requests/updateTranslatedProject.ts
@@ -3,6 +3,7 @@
 
 // firebase
 import {onRequest} from "firebase-functions/v2/https";
+import {log} from "firebase-functions/logger";
 
 // infra
 import {firestore} from "../infra/cloudFunctions";
@@ -14,15 +15,17 @@ export const updateTranslatedProject = onRequest((request, response) => {
   try {
     const data: Project = request.body;
     const projectId = data.id;
-    const status = data.status;
+    const newStatus = data.status;
     const translatedFileLink = data.translatedFileLink;
+    log(`Got request body data. Start updating project with id: ${projectId}`);
 
     // Update project's status and translated file link
     const projectRef = firestore.collection("projects").doc(projectId);
     projectRef.update({
-      status: status,
+      status: newStatus,
       translatedFileLink: translatedFileLink,
     });
+    log(`Project with id: ${projectId} was updated with { status: ${newStatus}, translatedFileLink: ${translatedFileLink} }`);
 
     response.status(200).send("Translated project status updated successfully!");
   } catch (err: any) {
