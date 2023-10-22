e9b65901944623243e168a33f62e87550ddea403
Syrnnik <sakh.alexandr@gmail.com>
2023-10-22 19:53:29 +0300

    fix: remove config

--- functions/src/infra/cloudFunctions.ts
+++ functions/src/infra/cloudFunctions.ts
@@ -1,8 +1,5 @@
 import * as admin from "firebase-admin";
-import * as functions from "firebase-functions";
 
-export const config = functions.config();
-
-admin.initializeApp(config.firebase);
+admin.initializeApp();
 
 export const firestore = admin.firestore();
