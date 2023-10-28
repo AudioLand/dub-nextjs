8fc1d6d5a664f58fcf488f69cb2bc91d06c12b8b
Syrnnik <sakh.alexandr@gmail.com>
2023-10-28 16:50:56 +0300

    feat: add downloading translated file

--- src/components/projects/ProjectsTable.tsx
+++ src/components/projects/ProjectsTable.tsx
@@ -1,21 +1,14 @@
 // react
+import { saveAs } from "file-saver";
 import { FC } from "react";
 import Badge from "~/core/ui/Badge";
 
 // ui-components
 import Button from "~/core/ui/Button";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "~/core/ui/Table";
-
-import PROJECT_STATUSES from "~/lib/projects/statuses";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "~/core/ui/Table";
 
 // types
+import PROJECT_STATUSES from "~/lib/projects/statuses";
 import { Project } from "~/lib/projects/types/project";
 
 interface ProjectsTableProps {
@@ -41,6 +34,15 @@ export const ProjectsTable: FC<ProjectsTableProps> = (props) => {
     }
   };
 
+  const isButtonDisabled = (projectStatus: PROJECT_STATUSES) => {
+    return projectStatus !== PROJECT_STATUSES.translated;
+  };
+
+  const handleDownloadTranslatedFile = (translatedFileLink: string) => {
+    const filename = translatedFileLink.split("/").pop();
+    saveAs(translatedFileLink, filename);
+  };
+
   return (
     <Table>
       <TableHeader>
@@ -54,7 +56,7 @@ export const ProjectsTable: FC<ProjectsTableProps> = (props) => {
       </TableHeader>
 
       <TableBody>
-        {projects?.map(({ id, name, targetLanguage, status, createdAt }) => (
+        {projects?.map(({ id, name, targetLanguage, translatedFileLink, status, createdAt }) => (
           <TableRow key={id}>
             {/* Project Fields Values */}
             <TableCell>{name}</TableCell>
@@ -69,12 +71,13 @@ export const ProjectsTable: FC<ProjectsTableProps> = (props) => {
 
             {/* Project Buttons */}
             <TableCell className="flex flex-row gap-5">
-              <Button variant="destructive">Remove</Button>
-              <Button>Download</Button>
               <Button
-                disabled={status !== PROJECT_STATUSES.translated}
-                href={`/projects/${id}`}
+                disabled={isButtonDisabled(status)}
+                onClick={() => handleDownloadTranslatedFile(translatedFileLink)}
               >
+                Download
+              </Button>
+              <Button disabled={isButtonDisabled(status)} href={`/projects/${id}`}>
                 View
               </Button>
             </TableCell>
