22c4e205006ac3b4a9f14ebeca36b5f0517d81c5
Syrnnik <sakh.alexandr@gmail.com>
2023-11-04 13:31:04 +0300

    fix: mp3 file type for uploading

--- src/lib/projects/limits.ts
+++ src/lib/projects/limits.ts
@@ -1,3 +1,3 @@
 export const MAX_PROJECTS_COUNT_FOR_FREE_PLAN = 3;
 
-export const ACCEPTED_FILES = ["audio/mp3", "audio/mp4", "video/avi", "video/mp4"];
+export const ACCEPTED_FILES = ["audio/mpeg", "audio/mp4", "video/avi", "video/mp4"];
