e92eb226ddee6ab09158a6e64df77bbdc83e5dbb
Syrnnik <sakh.alexandr@gmail.com>
2023-11-08 16:11:53 +0300

    feat: update file duration hook to get from plan

--- src/lib/projects/hooks/use-max-media-file-duration.ts
+++ src/lib/projects/hooks/use-max-media-file-duration.ts
@@ -1,28 +1,13 @@
-import flagsmith from "flagsmith";
-import FEATURES_IDS_LIST from "~/core/flagsmith/features-ids-list";
-import useIsSubscriptionActive from "~/lib/organizations/hooks/use-is-subscription-active";
+// hooks
+import useUserSubscriptionPlan from "~/lib/user/hooks/use-user-subscription-plan";
 
 const useMaxMediaFileDuration = () => {
-  const isSubscriptionActive = useIsSubscriptionActive();
+  const { userSubscriptionPlan } = useUserSubscriptionPlan();
 
-  if (!isSubscriptionActive) {
-    return {
-      inMinutes: 1,
-      inSeconds: 60,
-    };
-  }
-
-  const valueString: string = flagsmith.getValue(
-    FEATURES_IDS_LIST.max_media_file_duration_in_minutes,
-  );
-  const maxMediaFileDurationInMinutes = Number(valueString);
-
-  const MAX_MEDIA_FILE_DURATION = {
-    inMinutes: maxMediaFileDurationInMinutes,
-    inSeconds: maxMediaFileDurationInMinutes * 60,
+  return {
+    inMinutes: userSubscriptionPlan.maxFileDurationInMinutes,
+    inSeconds: userSubscriptionPlan.maxFileDurationInMinutes * 60,
   };
-
-  return MAX_MEDIA_FILE_DURATION;
 };
 
 export default useMaxMediaFileDuration;
