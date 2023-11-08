63690349c9634493ca01a06fd5a6121137fe85c2
Syrnnik <sakh.alexandr@gmail.com>
2023-11-08 16:13:25 +0300

    feat: update tokens info card logic

--- src/components/subscriptions/TokensInfoCard.tsx
+++ src/components/subscriptions/TokensInfoCard.tsx
@@ -1,116 +1,57 @@
-// hooks
-import { useCurrentOrganization } from "~/lib/organizations/hooks/use-current-organization";
+// react
 
-// constants
+// ui-components
 import { Progress, ProgressIndicator } from "@radix-ui/react-progress";
-import { FC, useState } from "react";
-import configuration from "~/configuration";
 import Button from "~/core/ui/Button";
 import If from "~/core/ui/If";
 
+// hooks
+import useUserTokens from "~/lib/user/hooks/use-user-tokens";
+
+// constants
+import useUserSubscriptionPlan from "~/lib/user/hooks/use-user-subscription-plan";
+
 const TokensInfoCard = () => {
-  // TODO: get used tokens from firebase
-  const userOrganization = useCurrentOrganization();
+  const { userSubscriptionPlan, isFreePlan } = useUserSubscriptionPlan();
+  const { usedTokens, availableTokensInSubscription, fetchUsedTokensStatus } = useUserTokens();
 
-  if (!userOrganization) return <></>;
+  if (fetchUsedTokensStatus === "loading") return <></>;
 
-  const userSubscription = userOrganization.subscription;
-  const usedTokensInSeconds = userOrganization.usedTokensInSeconds! || 0;
-
-  //* For free plan
-  if (!userSubscription) {
-    const subscriptionProduct = configuration.stripe.products[0];
-
-    return (
-      <CardTemplate
-        subscriptionName={subscriptionProduct.name}
-        usedTokensInSeconds={usedTokensInSeconds}
-        totalSubscriptionTokens={subscriptionProduct.tokens!}
-      />
-    );
-  }
-
-  //* For other plans
-  const subscriptionProductId = userSubscription.product;
-  const subscriptionProduct = configuration.stripe.products.find(
-    (product) => product.id === subscriptionProductId,
-  )!;
-
-  const totalSubscriptionTokens = subscriptionProduct.tokens!;
-
-  return (
-    <CardTemplate
-      subscriptionName={subscriptionProduct.name}
-      usedTokensInSeconds={usedTokensInSeconds}
-      totalSubscriptionTokens={totalSubscriptionTokens}
-    />
-  );
-};
-
-export default TokensInfoCard;
-
-interface CardTemplateProps {
-  subscriptionName: string;
-  usedTokensInSeconds: number;
-  totalSubscriptionTokens: number;
-}
-
-const CardTemplate: FC<CardTemplateProps> = (props) => {
-  const { subscriptionName, usedTokensInSeconds, totalSubscriptionTokens } = props;
-
-  const [isShowDetails, setShowDetails] = useState<boolean>(false);
-
-  const roundedUsedTokens = Math.ceil(usedTokensInSeconds / 60);
-  const usedTokensMinutes = Math.floor(usedTokensInSeconds / 60);
-  const usedTokensSeconds = usedTokensInSeconds % 60;
-  const progressIndicatorWidthInPercentage = (roundedUsedTokens / totalSubscriptionTokens) * 100;
-
-  const showUpgradePlanButton = progressIndicatorWidthInPercentage > 65;
-
-  const handleShowDetailsOnHover = () => {
-    setShowDetails(true);
-  };
-
-  const handleHideDetailsOnHover = () => {
-    setShowDetails(false);
-  };
+  const progressIndicatorWidthInPercentage =
+    (usedTokens.roundedInMinutes / availableTokensInSubscription.inMinutes) * 100;
+  //* Show upgrade button if tokens less than 35%
+  const showUpgradePlanButton = progressIndicatorWidthInPercentage > 65 || isFreePlan;
 
   return (
     <div className="flex flex-col gap-3">
-      <div
-        className="flex flex-col gap-2 py-2 pb-3 px-4 bg-gray-900 rounded"
-        onMouseEnter={handleShowDetailsOnHover}
-        onMouseLeave={handleHideDetailsOnHover}
-      >
+      <div className="flex flex-col gap-2 py-2 pb-3 px-4 bg-gray-900 rounded">
         <div className="flex justify-between">
           {/* Subscription Name */}
-          <span className="text-lg font-semibold">{subscriptionName}</span>
+          <span className="text-lg font-semibold">{userSubscriptionPlan.name}</span>
 
           {/* Used tokens info */}
           <div className="flex items-center gap-1 text-md">
-            <span>{roundedUsedTokens}</span>
+            <span>{usedTokens.roundedInMinutes}</span>
             <span>/</span>
-            <span>{totalSubscriptionTokens}</span>
+            <span>{availableTokensInSubscription.inMinutes}</span>
             <span>tokens</span>
           </div>
         </div>
 
         <Progress className="h-2 w-full overflow-hidden rounded-full bg-gray-800">
           <ProgressIndicator
+            className="h-2 bg-purple-700 duration-300 ease-in-out"
             style={{
               width: `${progressIndicatorWidthInPercentage}%`,
             }}
-            className="h-full bg-purple-500 duration-300 ease-in-out bg-white"
           />
         </Progress>
 
-        <If condition={isShowDetails}>
-          <div className="flex mt-1 text-gray-500 text-sm">
-            {`Used ${usedTokensMinutes} min ${
-              usedTokensSeconds > 0 && usedTokensSeconds + " sec "
-            }from ${totalSubscriptionTokens} min`}
-          </div>
-        </If>
+        <div className="flex mt-1 text-gray-500 text-sm">
+          {usedTokens.secondsLeft > 0
+            ? `Used ${usedTokens.roundedInMinutes} min ${usedTokens.secondsLeft} sec from ${availableTokensInSubscription.inMinutes} min`
+            : `Used ${usedTokens.roundedInMinutes} min from ${availableTokensInSubscription.inMinutes} min`}
+        </div>
       </div>
 
       <If condition={showUpgradePlanButton}>
@@ -121,3 +62,5 @@ const CardTemplate: FC<CardTemplateProps> = (props) => {
     </div>
   );
 };
+
+export default TokensInfoCard;
