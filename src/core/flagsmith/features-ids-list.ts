3a889d1acec139ebee21f4404bb7233367576352
Syrnnik <sakh.alexandr@gmail.com>
2023-11-08 16:16:07 +0300

    refactor: remove unused feature flag

--- src/core/flagsmith/features-ids-list.ts
+++ src/core/flagsmith/features-ids-list.ts
@@ -1,6 +1,5 @@
 const FEATURES_IDS_LIST = {
   languages_list: "languages_list",
-  max_media_file_duration_in_minutes: "max_media_file_duration_in_minutes",
   requirements_info_tooltip: "requirements_info_tooltip",
 };
 
