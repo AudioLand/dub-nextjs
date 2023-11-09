9c9f2fc393d7abeeded72f55d44ed7b1aad669a5
Syrnnik <sakh.alexandr@gmail.com>
2023-11-09 14:22:54 +0300

    fix: firebase storage provider in settings page

--- src/pages/settings/profile/index.tsx
+++ src/pages/settings/profile/index.tsx
@@ -1,23 +1,23 @@
-import { useCallback, useContext } from "react";
+import { UserInfo } from "firebase/auth";
 import { GetServerSidePropsContext } from "next";
 import { Trans } from "next-i18next";
-import { UserInfo } from "firebase/auth";
+import { useCallback, useContext } from "react";
 import { useUser } from "reactfire";
 
-import { withAppProps } from "~/lib/props/with-app-props";
 import { UserSessionContext } from "~/core/contexts/user-session";
+import { withAppProps } from "~/lib/props/with-app-props";
 
-import UpdateProfileForm from "~/components/profile/UpdateProfileForm";
-import ProfileSettingsTabs from "~/components/profile/ProfileSettingsTabs";
-import SettingsPageContainer from "~/components/settings/SettingsPageContainer";
-import SettingsContentContainer from "~/components/settings/SettingsContentContainer";
-import SettingsTile from "~/components/settings/SettingsTile";
 import Head from "next/head";
+import ProfileSettingsTabs from "~/components/profile/ProfileSettingsTabs";
+import UpdateProfileForm from "~/components/profile/UpdateProfileForm";
+import SettingsContentContainer from "~/components/settings/SettingsContentContainer";
+import SettingsPageContainer from "~/components/settings/SettingsPageContainer";
+import SettingsTile from "~/components/settings/SettingsTile";
+import FirebaseStorageProvider from "~/core/firebase/components/FirebaseStorageProvider";
 
 type ProfileData = Partial<UserInfo>;
 
 const Profile = () => {
-  console.log("Profile провалились");
   const { userSession, setUserSession } = useContext(UserSessionContext);
   const { data: user } = useUser();
 
@@ -43,22 +43,24 @@ const Profile = () => {
   }
 
   return (
-    <SettingsPageContainer title={"Settings"}>
-      <Head>
-        <title key={"title"}>My Details</title>
-      </Head>
+    <FirebaseStorageProvider>
+      <SettingsPageContainer title={"Settings"}>
+        <Head>
+          <title key={"title"}>My Details</title>
+        </Head>
 
-      <ProfileSettingsTabs />
+        <ProfileSettingsTabs />
 
-      <SettingsContentContainer>
-        <SettingsTile
-          heading={<Trans i18nKey={"profile:generalTab"} />}
-          subHeading={<Trans i18nKey={"profile:generalTabSubheading"} />}
-        >
-          <UpdateProfileForm user={user} onUpdate={onUpdate} />
-        </SettingsTile>
-      </SettingsContentContainer>
-    </SettingsPageContainer>
+        <SettingsContentContainer>
+          <SettingsTile
+            heading={<Trans i18nKey={"profile:generalTab"} />}
+            subHeading={<Trans i18nKey={"profile:generalTabSubheading"} />}
+          >
+            <UpdateProfileForm user={user} onUpdate={onUpdate} />
+          </SettingsTile>
+        </SettingsContentContainer>
+      </SettingsPageContainer>
+    </FirebaseStorageProvider>
   );
 };
 
