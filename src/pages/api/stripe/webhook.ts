e2bcaa56e48982852daec12d5f6bf2b849538690
Syrnnik <sakh.alexandr@gmail.com>
2023-11-09 16:27:16 +0300

    feat: reset user tokens when subscription paid

--- src/pages/api/stripe/webhook.ts
+++ src/pages/api/stripe/webhook.ts
@@ -1,31 +1,32 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import type { Stripe } from 'stripe';
-import getRawBody from 'raw-body';
+import { NextApiRequest, NextApiResponse } from "next";
+import getRawBody from "raw-body";
+import type { Stripe } from "stripe";
 
-import logger from '~/core/logger';
-import { getStripeInstance } from '~/core/stripe/get-stripe';
-import { StripeWebhooks } from '~/core/stripe/stripe-webhooks.enum';
+import logger from "~/core/logger";
+import { getStripeInstance } from "~/core/stripe/get-stripe";
+import { StripeWebhooks } from "~/core/stripe/stripe-webhooks.enum";
 
 import {
   throwBadRequestException,
   throwInternalServerErrorException,
-} from '~/core/http-exceptions';
+} from "~/core/http-exceptions";
 
-import { withPipe } from '~/core/middleware/with-pipe';
-import { withMethodsGuard } from '~/core/middleware/with-methods-guard';
-import { withAdmin } from '~/core/middleware/with-admin';
-import { withExceptionFilter } from '~/core/middleware/with-exception-filter';
+import { withAdmin } from "~/core/middleware/with-admin";
+import { withExceptionFilter } from "~/core/middleware/with-exception-filter";
+import { withMethodsGuard } from "~/core/middleware/with-methods-guard";
+import { withPipe } from "~/core/middleware/with-pipe";
 
 import {
   deleteOrganizationSubscription,
+  resetTokensByCustomerId,
   setOrganizationSubscription,
   updateSubscriptionById,
-} from '~/lib/server/organizations/subscriptions';
+} from "~/lib/server/organizations/subscriptions";
 
-import { buildOrganizationSubscription } from '~/lib/stripe/build-organization-subscription';
+import { buildOrganizationSubscription } from "~/lib/stripe/build-organization-subscription";
 
-const SUPPORTED_HTTP_METHODS: HttpMethod[] = ['POST'];
-const STRIPE_SIGNATURE_HEADER = 'stripe-signature';
+const SUPPORTED_HTTP_METHODS: HttpMethod[] = ["POST"];
+const STRIPE_SIGNATURE_HEADER = "stripe-signature";
 
 // NB: we disable body parser to receive the raw body string. The raw body
 // is fundamental to verify that the request is genuine
@@ -40,13 +41,10 @@ const webhookSecretKey = process.env.STRIPE_WEBHOOK_SECRET as string;
 /**
  * @description Handle the webhooks from Stripe related to checkouts
  */
-async function checkoutWebhooksHandler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+async function checkoutWebhooksHandler(req: NextApiRequest, res: NextApiResponse) {
   if (!webhookSecretKey) {
     return throwInternalServerErrorException(
-      `The variable STRIPE_WEBHOOK_SECRET is unset. Please add the STRIPE_WEBHOOK_SECRET environment variable`
+      `The variable STRIPE_WEBHOOK_SECRET is unset. Please add the STRIPE_WEBHOOK_SECRET environment variable`,
     );
   }
 
@@ -60,17 +58,13 @@ async function checkoutWebhooksHandler(
   const rawBody = await getRawBody(req);
   const stripe = await getStripeInstance();
 
-  const event = stripe.webhooks.constructEvent(
-    rawBody,
-    signature,
-    webhookSecretKey
-  );
+  const event = stripe.webhooks.constructEvent(rawBody, signature, webhookSecretKey);
 
   logger.info(
     {
       type: event.type,
     },
-    `[Stripe] Received Stripe Webhook`
+    `[Stripe] Received Stripe Webhook`,
   );
 
   try {
@@ -79,9 +73,7 @@ async function checkoutWebhooksHandler(
         const session = event.data.object as Stripe.Checkout.Session;
         const subscriptionId = session.subscription as string;
 
-        const subscription = await stripe.subscriptions.retrieve(
-          subscriptionId
-        );
+        const subscription = await stripe.subscriptions.retrieve(subscriptionId);
 
         await onCheckoutCompleted(session, subscription);
 
@@ -103,6 +95,14 @@ async function checkoutWebhooksHandler(
 
         break;
       }
+
+      case StripeWebhooks.InvoicePaid: {
+        const invoice = event.data.object as Stripe.Invoice;
+
+        await onSubscriptionContinued(invoice);
+
+        break;
+      }
     }
 
     return respondOk(res);
@@ -111,7 +111,7 @@ async function checkoutWebhooksHandler(
       {
         type: event.type,
       },
-      `[Stripe] Webhook handling failed`
+      `[Stripe] Webhook handling failed`,
     );
 
     logger.debug(e);
@@ -120,14 +120,11 @@ async function checkoutWebhooksHandler(
   }
 }
 
-export default function stripeCheckoutsWebhooksHandler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default function stripeCheckoutsWebhooksHandler(req: NextApiRequest, res: NextApiResponse) {
   const handler = withPipe(
     withMethodsGuard(SUPPORTED_HTTP_METHODS),
     withAdmin,
-    checkoutWebhooksHandler
+    checkoutWebhooksHandler,
   );
 
   return withExceptionFilter(req, res)(handler);
@@ -140,7 +137,7 @@ export default function stripeCheckoutsWebhooksHandler(
  */
 async function onCheckoutCompleted(
   session: Stripe.Checkout.Session,
-  subscription: Stripe.Subscription
+  subscription: Stripe.Subscription,
 ) {
   const organizationId = session.client_reference_id as string;
   const customerId = session.customer as string;
@@ -163,6 +160,10 @@ async function onSubscriptionUpdated(subscription: Stripe.Subscription) {
   const subscriptionData = buildOrganizationSubscription(subscription);
 
   return updateSubscriptionById(subscription.id, subscriptionData);
+}
+
+async function onSubscriptionContinued(invoice: Stripe.Invoice) {
+  return resetTokensByCustomerId(invoice.customer as string);
 }
 
 function respondOk(res: NextApiResponse) {
