2834c5042b4b2720a9f35ffbb2d6c85e256e6a29
Syrnnik <sakh.alexandr@gmail.com>
2023-11-10 22:53:06 +0300

    fix: organization undefind in sidebar

--- src/components/layouts/sidebar/AppSidebar.tsx
+++ src/components/layouts/sidebar/AppSidebar.tsx
@@ -3,15 +3,19 @@ import TokensInfoCard from "~/components/subscriptions/TokensInfoCard";
 import { SidebarContext } from "~/core/contexts/sidebar";
 import If from "~/core/ui/If";
 import Sidebar from "~/core/ui/Sidebar";
+import { useCurrentOrganization } from "~/lib/organizations/hooks/use-current-organization";
 import AppSidebarNavigation from "./AppSidebarNavigation";
 
 const AppSidebar = () => {
   const { collapsed } = useContext(SidebarContext);
+  const userOrganization = useCurrentOrganization();
+
+  const showTokensInfoCard = userOrganization !== undefined && !collapsed;
 
   return (
     <Sidebar>
       <AppSidebarNavigation />
-      <If condition={!collapsed}>
+      <If condition={showTokensInfoCard}>
         <div className="absolute inset-x-4 bottom-48 h-16">
           <TokensInfoCard />
         </div>
