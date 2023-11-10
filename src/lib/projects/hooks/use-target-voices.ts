a0ca63a94fd5c7c983936c21b06600ac49633088
Syrnnik <sakh.alexandr@gmail.com>
2023-11-10 22:59:30 +0300

    feat: add hook to get voices from 11labs

---
+++ src/lib/projects/hooks/use-target-voices.ts
@@ -0,0 +1,42 @@
+// hooks
+import { useEffect } from "react";
+import useSWRMutation from "swr/mutation";
+import { useApiRequest } from "~/core/hooks/use-api";
+import { TargetVoice } from "../types/target-voice";
+
+const VOICES_11LABS_API_URL = "https://api.elevenlabs.io/v1/voices";
+
+const ELEVENLABS_API_KEY = process.env.NEXT_PUBLIC_ELEVENLABS_API_KEY;
+console.log(ELEVENLABS_API_KEY);
+
+const useTargetVoices = () => {
+  const fetcher = useApiRequest<{ voices: TargetVoice[] }>();
+
+  if (!ELEVENLABS_API_KEY) {
+    throw new Error("ElevenLabs api key is not defined");
+  }
+
+  const { trigger: fetchVoices, ...mutationState } = useSWRMutation(
+    VOICES_11LABS_API_URL,
+    (path) => {
+      return fetcher({
+        path,
+        method: "GET",
+        headers: {
+          "xi-api-key": ELEVENLABS_API_KEY,
+        },
+      });
+    },
+  );
+
+  useEffect(() => {
+    fetchVoices();
+  }, [fetchVoices]);
+
+  return {
+    targetVoices: mutationState.data?.voices || [],
+    fetchTargetVoicesStatus: mutationState,
+  };
+};
+
+export default useTargetVoices;
