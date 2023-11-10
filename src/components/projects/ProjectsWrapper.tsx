2ca56f80e2f5f576b99e0fdd335758e47b743d42
Syrnnik <sakh.alexandr@gmail.com>
2023-11-11 01:27:37 +0300

    fix: move requirements info tooltip into heading

--- src/components/projects/ProjectsWrapper.tsx
+++ src/components/projects/ProjectsWrapper.tsx
@@ -4,14 +4,24 @@ import { FC, useState } from "react";
 // ui-components
 import Alert, { AlertHeading } from "~/core/ui/Alert";
 import Button from "~/core/ui/Button";
+import If from "~/core/ui/If";
 import Modal from "~/core/ui/Modal";
 import Spinner from "~/core/ui/Spinner";
+import { Tooltip, TooltipContent, TooltipTrigger } from "~/core/ui/Tooltip";
 import CreateProjectForm from "./CreateProjectForm";
 import ProjectsList from "./ProjectsList";
 
 // hooks
 import { useUserId } from "~/core/hooks/use-user-id";
 import useIsUserCanCreateDubs from "~/lib/projects/hooks/use-is-user-can-create-dubs";
+import useMaxMediaFileDuration from "~/lib/projects/hooks/use-max-media-file-duration";
+import useRequirementsInfoTooltipText from "~/lib/projects/hooks/use-requirements-info-tooltip-text";
+
+// constants
+import { MAX_FILE_DURATION_STRING_TEMPLATE } from "~/lib/projects/limits";
+
+// icons
+import { InformationCircleIcon } from "@heroicons/react/24/outline";
 
 const ProjectsWrapper = () => {
   const userId = useUserId();
@@ -41,7 +51,7 @@ const ProjectsWrapper = () => {
       </div>
 
       <Modal
-        heading="New Project"
+        heading={<CreateProjectModalHeading />}
         isOpen={isCreateProjectModalOpen}
         setIsOpen={setCreateProjectModalOpen}
       >
@@ -85,3 +95,72 @@ const FreePlanExceededAlert = () => (
     </div>
   </Alert>
 );
+
+const CreateProjectModalHeading = () => {
+  const { isInfoTooltipEnabled, infoTooltipTexts } = useRequirementsInfoTooltipText();
+  const MAX_MEDIA_FILE_DURATION = useMaxMediaFileDuration();
+
+  if (!infoTooltipTexts) {
+    return <span>New Project</span>;
+  }
+
+  //* Supported Languages
+  const supportedLanguagesSection = infoTooltipTexts.supported_languages;
+
+  // For source file
+  const forSourceFile = supportedLanguagesSection.for_source_file;
+  const sourceFileSupportedLanguages = forSourceFile.languages_list.join(", ");
+
+  // For export file
+  const forExportFile = supportedLanguagesSection.for_export_file;
+  const exportFileSupportedLanguages = forExportFile.languages_list.join(", ");
+
+  //* Recommendations
+  const recommendationsSection = infoTooltipTexts.recommendations;
+  const recommededPointsList = recommendationsSection.recommendations_list.map(
+    (recommededPoint) => {
+      if (recommededPoint.includes(MAX_FILE_DURATION_STRING_TEMPLATE)) {
+        return recommededPoint.replace(
+          MAX_FILE_DURATION_STRING_TEMPLATE,
+          MAX_MEDIA_FILE_DURATION.inMinutes.toString(),
+        );
+      }
+      return recommededPoint;
+    },
+  );
+
+  return (
+    <div className="flex items-center gap-2">
+      <span>New Project</span>
+      {/* Requirements list */}
+      <If condition={isInfoTooltipEnabled}>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <span>
+              <InformationCircleIcon className="h-6" color="gray" />
+            </span>
+          </TooltipTrigger>
+
+          <TooltipContent>
+            <div className="flex flex-col gap-3">
+              {/* Supported Languages */}
+              <div className="flex flex-col">
+                <span>{supportedLanguagesSection.title}:</span>
+                <span>{`- ${forSourceFile.title}: ${sourceFileSupportedLanguages}`}</span>
+                <span>{`- ${forExportFile.title}: ${exportFileSupportedLanguages}`}</span>
+              </div>
+
+              {/* Recommendations */}
+              <div className="flex flex-col">
+                <span>{recommendationsSection.title}:</span>
+                {recommededPointsList.map((recommendedPoint) => (
+                  <span key={recommendedPoint}>- {recommendedPoint}</span>
+                ))}
+              </div>
+            </div>
+          </TooltipContent>
+        </Tooltip>
+      </If>
+    </div>
+  );
+};
