9b0b66324e838f8c2e6bcffdf049cb6ebbd139f6
Syrnnik <sakh.alexandr@gmail.com>
2023-11-11 20:32:47 +0300

    fix: update subscribing form component

--- src/components/NewsletterSignup.tsx
+++ src/components/NewsletterSignup.tsx
@@ -1,22 +1,25 @@
-import ConvertkitSignupForm from '~/components/newsletter/ConvertkitSignupForm';
-import configuration from '~/configuration';
-import Heading from '~/core/ui/Heading';
+import configuration from "~/configuration";
+import Heading from "~/core/ui/Heading";
+import EmailOctopusSignupForm from "./newsletter/EmailOctopusSignupForm";
 
 function NewsletterSignup() {
+  const emailOctopusFormId = configuration.site.emailOctopusFormId;
+  if (!emailOctopusFormId) {
+    throw new Error("Email Octopus form id is not defined");
+  }
+
   return (
-    <div className={'flex flex-col space-y-4'}>
+    <div className={"flex flex-col space-y-4"}>
       <div>
         <Heading type={6}>Subscribe to our Newsletter</Heading>
 
-        <div className={'text-sm text-gray-500 dark:text-gray-400'}>
+        <div className={"text-sm text-gray-500 dark:text-gray-400"}>
           Get the latest updates from our team.
         </div>
       </div>
 
       <div>
-        <ConvertkitSignupForm formId={configuration.site.convertKitFormId}>
-          Subscribe
-        </ConvertkitSignupForm>
+        <EmailOctopusSignupForm formId={emailOctopusFormId}>Subscribe</EmailOctopusSignupForm>
       </div>
     </div>
   );
