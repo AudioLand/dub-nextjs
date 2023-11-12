201367ca6115a54ef37ebd5ba6e6251b174fd5bb
Syrnnik <sakh.alexandr@gmail.com>
2023-11-13 01:18:03 +0300

    feat: add emails texts feature flag

--- src/core/flagsmith/features-ids-list.ts
+++ src/core/flagsmith/features-ids-list.ts
@@ -1,6 +1,17 @@
 const FEATURES_IDS_LIST = {
   languages_list: "languages_list",
   requirements_info_tooltip: "requirements_info_tooltip",
+
+  emailTexts: {
+    notification_of_a_failed_payment_attempt: "notification_of_a_failed_payment_attempt",
+    notification_of_successful_registration: "notification_of_successful_registration",
+    password_successfully_reset: "password_successfully_reset",
+    project_translation_error: "project_translation_error",
+    successful_purchase_of_tokens: "successful_purchase_of_tokens",
+    successful_subscription: "successful_subscription",
+    successful_translation_of_the_project: "successful_translation_of_the_project",
+    warning_about_the_end_of_the_subscription: "warning_about_the_end_of_the_subscription",
+  },
 };
 
 export default FEATURES_IDS_LIST;
