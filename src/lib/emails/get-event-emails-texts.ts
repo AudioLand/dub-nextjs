341d0eb8fedee948f4da94274d667949b34534ae
Syrnnik <sakh.alexandr@gmail.com>
2023-11-14 22:28:10 +0300

    refactor: fetching email texts

--- src/lib/emails/get-event-emails-texts.ts
+++ src/lib/emails/get-event-emails-texts.ts
@@ -1,14 +1,21 @@
 import flagsmith from "flagsmith";
+
+// hooks
 import initFlagsmith from "~/core/flagsmith/hooks/init-flagsmith";
+
+// types
 import { EventEmail } from "./types/event-email";
 
-const getEventEmailText = (flagsmithTag: string) => {
-  initFlagsmith();
+const getEventEmailText = async (textsFlagId: string) => {
+  await initFlagsmith();
 
-  const valueString: string = flagsmith.getValue(flagsmithTag);
-  const eventEmail: EventEmail = JSON.parse(valueString);
-
-  return eventEmail;
+  const valueString: string = flagsmith.getValue(textsFlagId);
+  try {
+    const eventEmail: EventEmail = JSON.parse(valueString);
+    return eventEmail;
+  } catch (error: any) {
+    throw new Error("Event email texts is null, cannot fetch flagsmith");
+  }
 };
 
 export default getEventEmailText;
