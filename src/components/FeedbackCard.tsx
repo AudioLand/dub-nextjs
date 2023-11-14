44d4aa39ebc78f10ba02a8d4aa41151e9ba581e7
Syrnnik <sakh.alexandr@gmail.com>
2023-11-15 00:00:40 +0300

    fix: key error in feedback card

--- src/components/FeedbackCard.tsx
+++ src/components/FeedbackCard.tsx
@@ -14,9 +14,9 @@ const FeedbackCard = ({ avatarUrl, name, rating, comment }: FeedbackCardType) =>
     const starArray = [];
     for (let i = 0; i < 5; i++) {
       if (i < rating) {
-        starArray.push(<FullStar width={20} height={20} />);
+        starArray.push(<FullStar key={i} width={20} height={20} />);
       } else {
-        starArray.push(<EmptyStar width={20} height={20} />);
+        starArray.push(<EmptyStar key={i} width={20} height={20} />);
       }
     }
     return starArray;
@@ -26,7 +26,7 @@ const FeedbackCard = ({ avatarUrl, name, rating, comment }: FeedbackCardType) =>
     <div className="sm:max-w-[400px] w-[90vw] min-h-60 sm:mx-5 mb-10 mx-0 rounded-3xl p-10 shadow-[0_0px_20px_rgba(0,0,0,0.3)] shadow-violet-700">
       <div className="flex flex-row items-center">
         <Image src={avatarUrl} alt="avatar" width={40} height={40} />
-        <div  className="ml-5">
+        <div className="ml-5">
           <p>{name}</p>
           <div className="flex flex-row mt-3">{getStar()}</div>
         </div>
