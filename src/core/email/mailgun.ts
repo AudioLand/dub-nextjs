280e6f1908cb912335cb42c47fde0b0771e01f5c
Syrnnik <sakh.alexandr@gmail.com>
2023-11-15 14:53:51 +0300

    fix: api key naming

--- src/core/email/mailgun.ts
+++ src/core/email/mailgun.ts
@@ -3,15 +3,15 @@ import Mailgun from "mailgun.js";
 
 const mailgun = new Mailgun(formData);
 
-const MAILGUN_APY_KEY = process.env.NEXT_PUBLIC_MAILGUN_API_KEY;
+const MAILGUN_API_KEY = process.env.NEXT_PUBLIC_MAILGUN_API_KEY;
 
-if (!MAILGUN_APY_KEY) {
+if (!MAILGUN_API_KEY) {
   throw new Error("Mailgun API key is not defined");
 }
 
 export const mg = mailgun.client({
   username: "api",
-  key: MAILGUN_APY_KEY,
+  key: MAILGUN_API_KEY,
 });
 
 const MAILING_LIST_ENV = process.env.NEXT_PUBLIC_MAILGUN_MAILING_LIST;
