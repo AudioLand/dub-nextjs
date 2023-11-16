98b9de5d1c40df5e45dc3b880a984ba8bc749b49
Syrnnik <sakh.alexandr@gmail.com>
2023-11-16 20:08:31 +0300

    fix: update use target voices hook

--- src/lib/projects/hooks/use-target-voices.ts
+++ src/lib/projects/hooks/use-target-voices.ts
@@ -1,41 +1,12 @@
-// hooks
-import { useEffect } from "react";
-import useSWRMutation from "swr/mutation";
-import { useApiRequest } from "~/core/hooks/use-api";
+import { LANGUAGES_AND_VOICES_CONFIG } from "../languages-and-voices-config";
 import { TargetVoice } from "../types/target-voice";
 
-const VOICES_11LABS_API_URL = "https://api.elevenlabs.io/v1/voices";
-
-const ELEVENLABS_API_KEY = process.env.NEXT_PUBLIC_ELEVENLABS_API_KEY;
-
-const useTargetVoices = () => {
-  const fetcher = useApiRequest<{ voices: TargetVoice[] }>();
-
-  if (!ELEVENLABS_API_KEY) {
-    throw new Error("ElevenLabs api key is not defined");
-  }
-
-  const { trigger: fetchVoices, ...mutationState } = useSWRMutation(
-    VOICES_11LABS_API_URL,
-    (path) => {
-      return fetcher({
-        path,
-        method: "GET",
-        headers: {
-          "xi-api-key": ELEVENLABS_API_KEY,
-        },
-      });
-    },
+const useTargetVoices = (currentLanguage: string) => {
+  const availableVoices: TargetVoice[] = LANGUAGES_AND_VOICES_CONFIG.filter((voice) =>
+    voice.languages.includes(currentLanguage.toLocaleLowerCase()),
   );
 
-  useEffect(() => {
-    fetchVoices();
-  }, [fetchVoices]);
-
-  return {
-    targetVoices: mutationState.data?.voices || [],
-    fetchTargetVoicesStatus: mutationState,
-  };
+  return availableVoices;
 };
 
 export default useTargetVoices;
