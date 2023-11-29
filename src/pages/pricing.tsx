c35a0cb46af0ca9fda0ed54bd572ed5b9f0fb455
mskKote <msk.vitaly@gmail.com>
2023-11-29 02:05:25 +0100

    fix: pricing texts (GEN-228)

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -1,35 +1,35 @@
-import Head from 'next/head';
-import { GetStaticPropsContext } from 'next';
+import Head from "next/head";
+import { GetStaticPropsContext } from "next";
 
-import { withTranslationProps } from '~/lib/props/with-translation-props';
+import { withTranslationProps } from "~/lib/props/with-translation-props";
 
-import configuration from '~/configuration';
-import SiteHeader from '~/components/SiteHeader';
+import configuration from "~/configuration";
+import SiteHeader from "~/components/SiteHeader";
 
-import Layout from '~/core/ui/Layout';
-import Container from '~/core/ui/Container';
-import SubHeading from '~/core/ui/SubHeading';
-import Footer from '~/components/Footer';
-import PricingTable from '~/components/PricingTable';
-import Heading from '~/core/ui/Heading';
+import Layout from "~/core/ui/Layout";
+import Container from "~/core/ui/Container";
+import SubHeading from "~/core/ui/SubHeading";
+import Footer from "~/components/Footer";
+import PricingTable from "~/components/PricingTable";
+import Heading from "~/core/ui/Heading";
 
 const Pricing = () => {
   return (
     <>
       <Layout>
         <Head>
-          <title key="title">
-            {`Pricing - ${configuration.site.siteName}`}
-          </title>
+          <title key="title">{`Pricing - ${configuration.site.siteName}`}</title>
         </Head>
 
         <SiteHeader />
 
         <Container>
-          <div className={'flex flex-col space-y-8 my-8'}>
-            <div className={'flex flex-col items-center space-y-4'}>
-              <Heading type={1}>Pricing</Heading>
-              <SubHeading>Fair pricing for your customers</SubHeading>
+          <div className={"flex flex-col space-y-8 my-8"}>
+            <div className={"flex flex-col items-center space-y-4"}>
+              <Heading type={1}>Unleash the full power of Audioland Dubbing</Heading>
+              <SubHeading>
+                Access all the features you need with one of our premium plans
+              </SubHeading>
             </div>
 
             <PricingTable />
