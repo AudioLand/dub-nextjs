85d44570fdf08f9785cc505ab90f205e22173cd8
Syrnnik <sakh.alexandr@gmail.com>
2023-12-29 00:57:53 +0300

    refactor: pipelines urls

--- src/core/ml-pipeline/url.ts
+++ src/core/ml-pipeline/url.ts
@@ -1,7 +1,2 @@
-const PIPELINE_PRODUCTION_URL = "https://audioland.fly.dev";
-const PIPELINE_DEVELOPMENT_URL = "http://localhost:8080";
-
-const PIPELINE_URL =
-  process.env.NODE_ENV == "development" ? PIPELINE_DEVELOPMENT_URL : PIPELINE_PRODUCTION_URL;
-
-export default PIPELINE_URL;
+export const OUR_PIPELINE_URL = "https://audioland.fly.dev";
+export const RASK_PIPELINE_URL = "https://audioland-rask.fly.dev";
