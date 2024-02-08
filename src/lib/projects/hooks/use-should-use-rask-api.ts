996f04af8ed2f951fcc23d279ee98a03119bfc54
Syrnnik <sakh.alexandr@gmail.com>
2024-02-08 16:35:09 +1100

    refactor: some alex api vars naming

--- src/lib/projects/hooks/use-should-use-rask-api.ts
+++ src/lib/projects/hooks/use-should-use-rask-api.ts
@@ -4,9 +4,9 @@ import flagsmith from "flagsmith";
 // constants
 import FEATURES_IDS_LIST from "~/core/flagsmith/features-ids-list";
 
-const useShouldUseRaskAPI = () => {
-  const shouldUseRaskAPI: boolean = flagsmith.hasFeature(FEATURES_IDS_LIST.use_rask_api);
-  return shouldUseRaskAPI;
+const useshouldUseAlexAPI = () => {
+  const shouldUseAlexAPI: boolean = flagsmith.hasFeature(FEATURES_IDS_LIST.use_rask_api);
+  return shouldUseAlexAPI;
 };
 
-export default useShouldUseRaskAPI;
+export default useshouldUseAlexAPI;
