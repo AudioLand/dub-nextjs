cae49b4e30eb5aef6842b6b753d6f32065a4b36f
Syrnnik <sakh.alexandr@gmail.com>
2024-02-14 01:08:36 +1100

    fix: hide auth & email settings tab for sumo-lings

--- src/components/profile/ProfileSettingsTabs.tsx
+++ src/components/profile/ProfileSettingsTabs.tsx
@@ -1,51 +1,49 @@
-import NavigationItem from '~/core/ui/Navigation/NavigationItem';
-import NavigationMenu from '~/core/ui/Navigation/NavigationMenu';
-import MobileNavigationDropdown from '~/core/ui/MobileNavigationMenu';
+import If from "~/core/ui/If";
+import MobileNavigationDropdown from "~/core/ui/MobileNavigationMenu";
+import NavigationItem from "~/core/ui/Navigation/NavigationItem";
+import NavigationMenu from "~/core/ui/Navigation/NavigationMenu";
+import { useIsSumolingUser } from "~/lib/appsumo/hooks/is-sumo-ling-user";
 
 const links = {
   General: {
-    path: '/settings/profile',
-    label: 'profile:generalTab',
+    path: "/settings/profile",
+    label: "profile:generalTab",
   },
   Authentication: {
-    path: '/settings/profile/authentication',
-    label: 'profile:authenticationTab',
+    path: "/settings/profile/authentication",
+    label: "profile:authenticationTab",
   },
   Email: {
-    path: '/settings/profile/email',
-    label: 'profile:emailTab',
+    path: "/settings/profile/email",
+    label: "profile:emailTab",
   },
   Password: {
-    path: '/settings/profile/password',
-    label: 'profile:passwordTab',
+    path: "/settings/profile/password",
+    label: "profile:passwordTab",
   },
 };
 
 const ProfileSettingsTabs = () => {
+  const isSumolingUser = useIsSumolingUser();
   const itemClassName = `flex justify-center lg:justify-start items-center w-full`;
 
   return (
     <>
-      <div className={'hidden w-[12rem] lg:flex'}>
+      <div className={"hidden w-[12rem] lg:flex"}>
         <NavigationMenu vertical pill>
-          <NavigationItem
-            className={itemClassName}
-            link={links.General}
-            depth={0}
-          />
+          <NavigationItem className={itemClassName} link={links.General} depth={0} />
 
-          <NavigationItem
-            className={itemClassName}
-            link={links.Authentication}
-          />
+          <If condition={!isSumolingUser}>
+            <NavigationItem className={itemClassName} link={links.Authentication} />
 
-          <NavigationItem className={itemClassName} link={links.Email} />
+            <NavigationItem className={itemClassName} link={links.Email} />
+          </If>
 
           <NavigationItem className={itemClassName} link={links.Password} />
         </NavigationMenu>
       </div>
 
-      <div className={'block w-full lg:hidden'}>
+      <div className={"block w-full lg:hidden"}>
         <MobileNavigationDropdown links={Object.values(links)} />
       </div>
     </>
